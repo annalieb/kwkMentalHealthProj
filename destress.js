bf3a89bdf9901ee6e4c979c8da84eb72047c1101
Anna Lieb <53187262+annalieb@users.noreply.github.com>
2019-08-01 13:47:26 -0400

    Update destress.js

--- destress.js
+++ destress.js
@@ -1,25 +1,35 @@
 /* JS for de-stress page */
 
 // QUIZ
-var option1 = $("#option1");
-console.log(option1);
+var QUIZ_OUTCOMES = ["You enjoy being active and getting connected with your body. Next time you feel down, try some yoga poses to center yourself. ",
+"You enjoy your relaxation time, but you also like to stay busy. If you're feeling sad or stressed, make some hot tea and ",
+"You are calm by nature and gravitate towards peaceful environments. When you feel overwhelmed, try some breathing exercises to refocus your energy. ",
+"You have a creative spirit. You're also good at concentrating on the things you care about. If you feel anxious, try drawing or coloring as a calm "]
+
+var resultText = $("#result");
 
 var submitQuiz = $("#submitQuiz");
 submitQuiz.on("click", getResult);
-console.log(document.getElementsByClassName("option1").value);
 
 function getResult() {  // repeat in for loop for all the questions?
   var total = 0;
-  if (document.getElementByClass("answer").value == "Option 1") {
-    total += 1;
-  } else if (document.getElementByClass("answer").value == "Option 2") {
-    total += 2;
-  } else if (document.getElementByClass("answer").value == "Option 3") {
-    total += 3;
-  } else if (document.getElementByClass("answer").value == "Option 4") {
-    total += 4;
+  for (var i = 1; i <= 4; i++) {
+    if (document.getElementById(`Q${i}OP1`).checked) {
+      total += 1;
+    } else if (document.getElementById(`Q${i}OP2`).checked) {
+      total += 2;
+    } else if (document.getElementById(`Q${i}OP3`).checked) {
+      total += 3;
+    } else if (document.getElementById(`Q${i}OP4`).checked) {
+      total += 4;
+    }
   }
-  var result = total / 4;
+
+  var result = Math.round (total / 4);
+  console.log(total);
+  console.log(result);
+  resultText.text(QUIZ_OUTCOMES[result]);
+
 }
 
 // POSITIVE MESSAGE GENERATOR
@@ -27,12 +37,14 @@ function getResult() {  // repeat in for loop for all the questions?
 var positivityMessages = [
   "Never give up",
   "Believe in yourself",
-  "Life is better with friends",
   "Your present situation is not your final destination",
   "The best is yet to come",
   "Mistakes can help you learn and grow",
   "Surround yourself with love & positivity",
-  "You are capable of amazing things"];
+  "You are capable of amazing things",
+  "Just keep going",
+"The rainbow only comes after rain",
+];
 
 var messageText = $("#messageOutput");
 
@@ -63,6 +75,7 @@ fullListButton.on("click", displayFullList);
 function displayFullList() {
   var output = $("#fullList");
   for (var i = 0; i < positivityMessages.length; i++) {
-    output.append(`<p>${positivityMessages[i]}</p>`)
+    output.append(`<p>${positivityMessages[i]}</p>`);
   }
 }
+
