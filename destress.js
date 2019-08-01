85142199f83030b3d234d80cc8c4439cd4293065
Anna Lieb <53187262+annalieb@users.noreply.github.com>
2019-08-01 08:52:41 -0400

    Update destress.js

--- destress.js
+++ destress.js
@@ -1,5 +1,29 @@
 /* JS for de-stress page */
 
+// QUIZ
+var option1 = $("#option1");
+console.log(option1);
+
+var submitQuiz = $("#submitQuiz");
+submitQuiz.on("click", getResult);
+console.log(document.getElementsByClassName("option1").value);
+
+function getResult() {  // repeat in for loop for all the questions?
+  var total = 0;
+  if (document.getElementByClass("answer").value == "Option 1") {
+    total += 1;
+  } else if (document.getElementByClass("answer").value == "Option 2") {
+    total += 2;
+  } else if (document.getElementByClass("answer").value == "Option 3") {
+    total += 3;
+  } else if (document.getElementByClass("answer").value == "Option 4") {
+    total += 4;
+  }
+  var result = total / 4;
+}
+
+// POSITIVE MESSAGE GENERATOR
+
 var positivityMessages = [
   "Never give up",
   "Believe in yourself",
@@ -33,7 +57,7 @@ function addMsg() {
 }
 
 var fullList = $("#fullList"); // the text
-var fullListButton = $("#fullListButton");
+var fullListButton = $("#fullListButton"); // the button
 fullListButton.on("click", displayFullList);
 
 function displayFullList() {
